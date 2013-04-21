bcffbe6959782e96e1e39b506c59ec35c7defa19
roglew <rglew56@gmail.com>
2013-04-20 21:05:26 -0400

    Began definitions, added Render.draw()

---
+++ src/render.cpp
@@ -0,0 +1,55 @@
+#include "render.h"
+#include <SFML/Graphics.hpp>
+
+static bool draw_event_lt(DrawEvent de1, DrawEvent de2)
+{
+	return (de1.depth < de2.depth);
+}
+
+// Private functions
+void Render::queue_draw_event(DrawEvent event)
+{
+	draw_queue.push_back(event);
+}
+
+// Public functions
+Render::Render(sf::RenderTarget* target)
+{
+	render_target = target;
+}
+
+void Render::render(sf::RenderWindow)
+{
+	sort(draw_queue.begin(), draw_queue.end());
+	for (int i=0; i<draw_queue.size(); i++)
+	{
+		switch(draw_queue[i].id)
+		{
+			case DRAW_DRAWABLE:
+				render_target->draw(*draw_queue[i].drawable);
+			break;
+
+			default:
+			break;
+		}
+	}
+}
+
+void Render::clear_queue()
+{
+	draw_queue.clear();
+}
+
+//void clear(sf::Color = sf::Color::Black, depth = 9999999999);
+
+void Render::draw(sf::Drawable* drawable, int depth)
+{
+	DrawEvent event;
+
+	event.id = DRAW_DRAWABLE;
+	event.depth = depth;
+	event.drawable = drawable;
+
+	draw_queue.push_back(event);
+}
+
--- src/render.h
+++ src/render.h
@@ -1,27 +1,37 @@
 #ifndef __RENDER_H__
 #define __RENDER_H__
 
-enum DrawEventId{DRAW_OBJECT, DRAW_SPRITE}
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+enum DrawEventId{DRAW_OBJECT, DRAW_DRAWABLE, DRAW_CLEAR,
+	DrawEventIdSize};
 
 struct DrawEvent
 {
 	DrawEventId id;
 	int depth;
-	double x;
-	double y;
-	sf::Sprite* sprite;
-}
+	sf::Drawable* drawable;
+	sf::Color color;
 
+	bool operator < (const DrawEvent& event)
+	{
+		return id < event.id;
+	}
+};
 
 class Render
 {
 	private:
-		vector<DrawEvent> draw_queue;
+		std::vector<DrawEvent> draw_queue;
+		sf::RenderTarget *render_target;
 
 		void queue_draw_event(DrawEvent);
 		// Adds a draw event to the queue
 
 	public:
+		Render(sf::RenderTarget*);
+
 		void render(sf::RenderWindow);
 		// MODIFIES: RenderWindow
 		// EFFECTS:  Draws everything in the draw queue to the window
@@ -30,11 +40,11 @@ class Render
 		// MODIFIES: this
 		// EFFECTS:  Clears the draw queue
 
-		void clear(sf::Color = sf::Color::Black, depth = 9999999999);
+		void clear(sf::Color = sf::Color::Black, int depth = 9999999999);
 		// MODIFIES: this
 		// EFFECTS:  clears the screen at the given depth
 
-		void draw_sprite(sf::Sprite*, depth = 0);
+		void draw(sf::Drawable*, int depth = 0);
 		// MODIFIES: this
 		// EFFECTS:  Draws the sprite at the given depth
 
