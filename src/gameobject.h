094de47ba05938c1a00e12244692c36bdb2a59ef
roglew <rglew56@gmail.com>
2013-04-22 00:41:26 -0400

    Added more to game object header file

--- src/gameobject.h
+++ src/gameobject.h
@@ -3,21 +3,68 @@
 
 #include "ids.h"
 #include "objectevent.hpp"
-#include <SFML/system.hpp>
+#include "sprite.h"
+#include <SFML/Graphics.hpp>
 #include <vector>
 
 class GameObject
 {
 	protected:
 		GameObjectId id;
+		int instance_id;
 		vector<ObjectEvent> events;
+
+		vector<vector<Sprite>> animations;
 		int depth;
 
-		Vector2<double> position;
-		Vector2f velocity;
-		float mass;
+		sf::Vector2<double> position;
+		sf::Vector2f velocity;
+		sf::Vector2f acceleration;
 
 	public:
+
+		/////////////////////
+		// Movement methods
+
+		void set_position(sf::Vector2f);
+		void set_position(sf::Vector2i);
+		void set_position(int x, int y);
+		// MODIFIES: Position
+		// EFFECTS:  Sets the position of the object
+
+		void move(sf::Vector2f);
+		void move(sf::Vector2i);
+		void move(int x, int y);
+		// MODIFIES: Position
+		// EFFECTS:  Moves the object by the given vector
+
+		void set_velocity(sf::Vector2f);
+		void set_velocity(sf::Vector2i);
+		void set_velocity(int x, int y);
+		// MODIFIES: Velocity
+		// EFFECTS:  Sets the velocity of the object
+
+		void add_velocity(sf::Vector2f);
+		void add_velocity(sf::Vector2i);
+		void add_velocity(int x, int y);
+		// MODIFIES: Velocity
+		// EFFECTS:  Adds to the velocity of the object
+
+		void set_acceleration(sf::Vector2f);
+		void set_acceleration(sf::Vector2i);
+		void set_acceleration(int x, int y);
+		// MODIFIES: Acceleration
+		// EFFECTS:  Sets the acceleration of the object
+
+		void add_acceleration(sf::Vector2f);
+		void add_acceleration(sf::Vector2i);
+		void add_acceleration(int x, int y);
+		// MODIFIES: Acceleration
+		// EFFECTS:  Adds to the acceleration of the object
+
+		//////////////////
+		// Event Methods
+
 		template <class T=bool>
 		void register_event(bool*, void (*response(T)));
 		void register_event(bool*, void (*response()));
@@ -37,6 +84,34 @@ class GameObject
 
 		void remove_event(bool*);
 		// EFFECTS: Removes any registered events that point to the given pointer
+
+		/////////////////////////////
+		// Sprite/Animation Methods
+
+		void next_frame();
+		// MODIFIES: This
+		// EFFECTS:  Changes sprite to the next frame in the current animation.
+		//           If the end of the animation is reached, sprite changes
+		//           to the first frame of the animation
+
+		void set_frame(int n);
+		// MODIFIES: This
+		// EFFECTS:  Sets the sprite to the nth frame of the current animation
+
+		void set_animation(int animation);
+		// MODIFIES: This
+		// EFFECTS:  Sets the current animation and changes to the first frame
+
+		sf::Sprite* get_current_frame();
+		// EFFECTS: Returns a pointer to a sf::Sprite that represents the current
+		//          frame of the current animation
+
+		sf::Sprite* get_frame(int n);
+		// EFFECTS: Returns a pointer to the sf::Sprite that represents the
+		//          nth frame of the current animation
+
+		sf::Sprite* get_frame(int animation, int n);
+		// EFFECTS: Returns the nth frame of the given animation
 
 };
 
