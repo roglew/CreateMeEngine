5e2f0e06888cabcc413449a8d11900cdc1f1b5cb
roglew <rglew56@gmail.com>
2013-04-21 20:53:27 -0400

    Split ObjectEvent into ObjectEvent/ObjectEventArg. ObjectEvent works

---
+++ src/objectevent.cpp
@@ -0,0 +1,61 @@
+#ifndef __OBJECTEVENT_HPP__
+#include "objectevent.hpp"
+#endif
+
+// Normal event
+ObjectEvent::ObjectEvent(bool* new_trigger, void (*new_response)())
+{
+	trigger = new_trigger;
+	response = new_response;
+}
+
+void ObjectEvent::set_trigger(bool* new_trigger)
+{
+	trigger = new_trigger;
+}
+
+void ObjectEvent::set_response(void (*new_response)())
+{
+	response = new_response;
+}
+
+void ObjectEvent::set(bool* new_trigger, void (*new_response)())
+{
+	trigger = new_trigger;
+	response = new_response;
+}
+
+void ObjectEvent::process()
+{
+	if (*trigger)
+		response();
+}
+
+// Response with argument
+template <class T>
+ObjectEventArg<T>::ObjectEventArg(bool* new_trigger, void (*new_response)(T))
+{
+	trigger = new_trigger;
+	arg_response = new_response;
+}
+
+template <class T>
+void ObjectEventArg<T>::set_response(void (*new_response)(T))
+{
+	arg_response = new_response;
+}
+
+template <class T>
+void ObjectEventArg<T>::set(bool* new_trigger, void (*new_response)(T))
+{
+	trigger = new_trigger;
+	arg_response = new_response;
+}
+
+template <class T>
+void ObjectEventArg<T>::process(T arg)
+{
+	if (*trigger)
+		arg_response(arg);
+}
+
--- src/objectevent.hpp
+++ src/objectevent.hpp
@@ -1,12 +1,63 @@
 #ifndef __OBJECTEVENT_HPP__
 #define __OBJECTEVENT_HPP__
 
-template <class T>
-struct ObjectEvent
+class ObjectEvent
 {
-	bool* trigger;
-	void (*response)(T);
+	private:
+		bool* trigger;
+		void (*response)();
+
+	public:
+		ObjectEvent(bool*, void (*response)());
+		// Creates an instance of an ObjectEvent that points to the given
+		// bool and associates it with the given response
+
+		void set_trigger(bool*);
+		// MODIFIES: this
+		// EFFECTS:  Sets the event trigger to a new bool
+
+		void set_response(void (*response)());
+		// MODIFIES: this
+		// EFFECTS:  Sets the event response to a new function
+
+		void set(bool*, void (*response)());
+		// MODIFIES: this
+		// EFFECTS:  Sets both the trigger and the response of the event
+
+		void process();
+		// EFFECTS: Checks if the value pointed to by the trigger is true.
+		//          If it is, it executes response() with the same argument
+		//          as process()
+
 };
+
+template <class T>
+class ObjectEventArg: private ObjectEvent
+{
+	private:
+		void (*arg_response)(T);
+
+	public:
+		ObjectEventArg(bool*, void (*response)(T));
+		// Creates an instance of an ObjectEvent that points to the given
+		// bool and associates it with the given response
+
+		void set_response(void (*response)(T));
+		// MODIFIES: this
+		// EFFECTS:  Sets the event response to a new function
+
+		void set(bool*, void (*response)(T));
+		// MODIFIES: this
+		// EFFECTS:  Sets both the trigger and the response of the event
+
+		void process(T);
+		// EFFECTS: Checks if the value pointed to by the trigger is true.
+		//          If it is, it executes response() with the same argument
+		//          as process()
+
+};
+
+#include "objectevent.cpp"
 
 #endif
 
