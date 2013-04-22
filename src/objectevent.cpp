e480a7543f85be24a1d53922712c4dbd725c8fd1
roglew <rglew56@gmail.com>
2013-04-21 23:56:35 -0400

    Fixed ObjectEvent::set()

--- src/objectevent.cpp
+++ src/objectevent.cpp
@@ -7,7 +7,6 @@ static void NOFUNCTION(){}
 template<class T>
 static void NOFUNCTION(T){}
 
-// Response with argument
 template <class T>
 ObjectEvent<T>::ObjectEvent(bool* new_trigger, void (*new_response)(T))
 {
@@ -24,11 +23,13 @@ ObjectEvent<T>::ObjectEvent(bool* new_trigger, void (*new_response)())
 	response_arg = NOFUNCTION;
 }
 
+
 template <class T>
 void ObjectEvent<T>::set_trigger(bool* new_trigger)
 {
 	trigger = new_trigger;
 }
+
 
 template <class T>
 void ObjectEvent<T>::set_response(void (*new_response)(T))
@@ -44,24 +45,35 @@ void ObjectEvent<T>::set_response(void (*new_response)())
 	response_arg = NOFUNCTION;
 }
 
+
 template <class T>
 void ObjectEvent<T>::set(bool* new_trigger, void (*new_response)(T))
 {
 	trigger = new_trigger;
-	response = new_response;
+	response = NOFUNCTION;
+	response_arg = new_response;
 }
+
+template <class T>
+void ObjectEvent<T>::set(bool* new_trigger, void (*new_response)())
+{
+	trigger = new_trigger;
+	response = new_response;
+	response_arg = NOFUNCTION;
+}
+
 
 template <class T>
 void ObjectEvent<T>::process(T arg)
 {
-	if (*trigger && response_arg)
+	if (*trigger)
 		response_arg(arg);
 }
 
 template <class T>
 void ObjectEvent<T>::process()
 {
-	if (*trigger && response)
+	if (*trigger)
 		response();
 }
 
