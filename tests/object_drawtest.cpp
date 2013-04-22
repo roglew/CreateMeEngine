f52b892277c225ff5577b60f8c1ab83a0d8a906b
roglew <rglew56@gmail.com>
2013-04-22 19:04:05 -0400

    Created test that implements drawing objects the way I want them to work

---
+++ tests/object_drawtest.cpp
@@ -0,0 +1,37 @@
+#include "../src/render.h"
+#include "../src/gameobject.hpp"
+#include "../src/sprite.h"
+#include <SFML/Graphics.hpp>
+
+int main(int argc, const char *argv[])
+{
+	GameObject obj;
+	sf::Sprite* spr = new sf::Sprite;
+	sf::Texture tex;
+	tex.load_from_file("resources/sprite.png");
+	spr->setTexture(tex);
+	int anim;
+	anim = obj.add_animation();
+	obj.append_frame(anim, spr);
+	obj.set_position(64, 64);
+
+	sf::RenderWindow window;
+	Render render(window);
+	Input input(window);
+	WindowStatus window_status;
+
+	while (window.isOpen())
+	{
+		input.update();
+		window_status = input.window_events();
+		if (window_status.closed())
+			window.close();
+		render.draw(object);
+		render.clear();
+		render.display();
+	}
+
+	delete spr;
+	return 0;
+}
+
