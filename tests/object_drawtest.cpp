5dbb46f0f1149212edc21181da3e808b2448fe97
roglew <rglew56@gmail.com>
2013-04-22 22:20:45 -0400

    Why the fuck did it work? It shouldnt be this easy. Well, object_drawtest works

--- tests/object_drawtest.cpp
+++ tests/object_drawtest.cpp
@@ -4,6 +4,8 @@
 #include "../src/input.h"
 #include "../src/render.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <cmath>
 
 int main(int argc, const char *argv[])
 {
@@ -17,20 +19,27 @@ int main(int argc, const char *argv[])
 	obj.append_frame(anim, spr);
 	obj.set_position(64, 64);
 
-	sf::RenderWindow window;
+	sf::RenderWindow window(sf::VideoMode(640, 480, 32), "Drawing an object");
+	window.setFramerateLimit(60);
 	Render render(window);
 	Input input(window);
 	WindowStatus window_status;
 
+	int i=0;
 	while (window.isOpen())
 	{
 		input.update();
 		window_status = input.window_events();
 		if (window_status.closed)
 			window.close();
+		
+		i++;
+		obj.set_position(320-64 + 64*std::cos(i/20.0), 240-64 + 64*std::sin(i/20.0));
+
 		render.draw(obj, -1);
 		render.clear();
 		render.render();
+		window.display();
 	}
 
 	delete spr;
