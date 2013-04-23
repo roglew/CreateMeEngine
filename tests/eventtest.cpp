e6299a6cd7439fb7e9807a205f58f7564a2f7ec3
roglew <rglew56@gmail.com>
2013-04-23 15:15:56 -0400

    I updated the event test. I still don't think it works though

--- tests/eventtest.cpp
+++ tests/eventtest.cpp
@@ -6,27 +6,18 @@ void response()
 	std::cout << "Responding\n";
 }
 
-void responsenum(int number)
-{
-	std::cout << "Number is: " << number << "\n";
-}
-
 int main(int argc, const char *argv[])
 {
 	bool trigger = false;
 
-	ObjectEvent<> event(&trigger, response);
-	ObjectEvent<int> eventarg(&trigger, responsenum);
+	ObjectEvent event(&trigger, response);
 
 	std::cout << "Processing with trigger=false\n";
 	event.process();
-	eventarg.process(5);
 
 	std::cout << "Processing with trigger=true\n";
 	trigger = true;
 	event.process();
-	eventarg.process(6);
-	
 	return 0;
 }
 
