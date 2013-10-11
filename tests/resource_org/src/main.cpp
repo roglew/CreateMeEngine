255d36f9bf81a821a314c775aa2cedc42b28700d
roglew <rglew56@gmail.com>
2013-10-11 16:57:36 -0400

    Made resource_org test use the engine

--- tests/resource_org/src/main.cpp
+++ tests/resource_org/src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-#include "resourcemanager.h"
+#include <SFML/Graphics.hpp>
+#include "render.h"
+#include "input.h"
+
 
 int main()
 {
-  std::cout << "Hello world\n";
+  Render render;
+  sf::RenderWindow *window = render_get_created_window();
+  Input input(*window);
+
+  while(window->isOpen()) {
+    input.update();
+    if (input.window.closed)
+      window->close();
+    render.clear();
+    render.render();
+  }
   return 0;
 }
