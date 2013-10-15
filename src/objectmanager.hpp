ecda2cc7cb66d545a33ea0acd78d9f86f9d19703
roglew <rglew56@gmail.com>
2013-10-15 15:37:20 -0400

    forgot objectmanager.hpp

---
+++ src/objectmanager.hpp
@@ -0,0 +1,39 @@
+#include <map>
+#include "game.h"
+#include "gameobject.h"
+#include "render.h"
+
+class ObjectManager
+{
+  private:
+    int current_obj_id;
+    map<unsigned int, *GameObject> object_list;
+    Game *game;
+  
+  public:
+    ObjectManager(Game* game);
+    // Constructor, needs a game to get info from
+    
+    ~ObjectManager();
+
+    template <class T>
+    unsigned int create_object(int x, int y);
+    // REQUIRES: T is a child of GameObject
+    // MODIFIES: This
+    // EFFECTS:  Stores the given object into the manager
+    
+    void destroy_object(unsigned int object_id);
+    // REQUIRES: Object is stored in the manager
+    // EFFECTS:  Destroys the object
+
+    void process_events();
+    // MODIFIES: Objects stored in this
+    // EFFECTS:  Performs process_events() for all of the objects stored in the
+    //           manager
+    
+    void draw_objects();
+    // MODIFIES: Render
+    // EFFECTS:  Draws the objects stored in manager to the render
+};
+
+#include "objectmanager.cpp"
