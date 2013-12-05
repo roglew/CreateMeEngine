b72d05fd5bfe0d5507d003a7fd12eff2ea5c0e4d
roglew <rglew56@gmail.com>
2013-12-05 17:07:31 -0500

    Squashed commit of the following:
    commit babf231f8a04280523ff2364f3f47f0fc53c1d11
    Author: roglew <rglew56@gmail.com>
    Date:   Thu Dec 5 17:04:56 2013 -0500
    
        Sprite class no longer has "loaded" value since update_texture handles loading
    
    commit 2dfe9b9f2471a7625f868653a97fe43f37c83ea8
    Author: roglew <rglew56@gmail.com>
    Date:   Thu Dec 5 16:53:43 2013 -0500
    
        Simplified setting the image of a sprite
    
    commit a7dfacfb711a85429038767c669b425c8ced0e18
    Author: roglew <rglew56@gmail.com>
    Date:   Thu Dec 5 16:53:23 2013 -0500
    
        Added basic int square struct

--- src/sprite.h
+++ src/sprite.h
@@ -2,21 +2,52 @@
 #define __SPRITE_H__
 
 #include "collision.h"
+#include "resourcemanager.h"
+#include "square.h"
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <string>
+
+#ifndef __RESOURCE_IDS__
+enum ResourceImage: unsigned int;
+enum ResourceSound: unsigned int;
+#endif
 
 class Sprite: public sf::Sprite
 {
   protected:
+    ResourceManager *resources;
     std::vector<Collision> collisions;
+    ResourceImage image;
+    Square image_pos;
+    bool is_part, image_defined;
   
   public:
+    Sprite(ResourceManager *resource_manager);
     // set_collision(const &Collision);
     // MODIFIES: This
     // EFFECTS:  Sets the collision struct of the sprite
 
     bool collides(Sprite& other_sprite);
     // EFFECTS: Returns whether the two sprites collide
+    
+    void set_image(ResourceImage image);
+    // MODIFIES: This
+    // Sets the image to the entire texture
+    
+    void set_image(ResourceImage image, Square &position);
+    void set_image(ResourceImage image, int x, int y, int w, int h);
+    // MODIFIES: This
+    // EFFECTS: Sets the image and section to load when load() is called
+
+    void update_texture();
+    // MODIFIES: This
+    // EFFECTS:  Updates the sfml texture to match the image. Also loads the image if
+    //           neccesary
+    
+    void load();
+    // REQUIRES: Image must have been set
+
 };
 
 
