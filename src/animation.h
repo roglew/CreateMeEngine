ecf733d7dbfc4044697945ba571a58e35398f81f
roglew <rglew56@gmail.com>
2013-12-07 20:32:03 -0500

    Updated header for animation create_from_strip

--- src/animation.h
+++ src/animation.h
@@ -6,6 +6,31 @@ enum ResourceImage: unsigned int;
 enum ResourceSound: unsigned int;
 #endif
 
+struct AnimationStripConfig
+{
+  int start_x;
+  int start_y;
+  int w;
+  int h;
+  int hsep;
+  int vsep;
+  int frames_per_row;
+  int count;
+
+  // Default Values
+  AnimationStripConfig()
+  {
+    start_x        = 0;
+    start_y        = 0;
+    w              = 32;
+    h              = 32;
+    hsep           = 0;
+    vsep           = 0;
+    frames_per_row = -1;
+    count          = 1;
+  }
+};
+
 Class Animation
 {
   protected:
@@ -21,8 +46,9 @@ Class Animation
     // EFFECTS:  Inserts a frame into the given animation at the given
     //           position (First frame is 0)
 
-    void generate_from_strip(sf::Texture texture, int start_x, int start_y, int w, int h,
-                             int hsep, int vsep, int frames_per_row, int count);
+    void generate_from_strip(ResourceImage image, AnimationStripConfig settings);
+    // MODIFIES: This
+    // EFFECTS:  Makes the animation from an animation strip
 
     Sprite* get_frame(int n);
     // EFFECTS: Returns a pointer to the nth frame
